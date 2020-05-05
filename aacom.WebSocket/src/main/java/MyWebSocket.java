e6f272472c0a52b3e06e1a6b21c1d0ef118418a4
Karl9Doniz <44242769+Karl9Doniz@users.noreply.github.com>
2020-05-05 12:55:31 +0300

    commited

--- aacom.WebSocket/src/main/java/MyWebSocket.java
+++ aacom.WebSocket/src/main/java/MyWebSocket.java
@@ -4,7 +4,7 @@ import netscape.javascript.JSObject;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
-
+//rrr
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
@@ -17,26 +17,38 @@ public class MyWebSocket extends WebSocketServer {
     private static final int PORT = 1111;
     private Set<WebSocket> cons; //set of connections
     private Map<String, String> users = new HashMap<>();
-    private static String ipinfo;
+    private Map<String, Boolean> authUsers = new HashMap<>();
 
     MyWebSocket() { //constructor
 
         super(new InetSocketAddress(PORT));
         cons = new HashSet<WebSocket>();
-
     }
 
     public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
         cons.add(webSocket); //add to set
         System.out.println("New connection from: " + webSocket.getRemoteSocketAddress().getAddress().getHostAddress());
         webSocket.send("Hello!");
+        String info = webSocket.getResourceDescriptor();
+        String token = info.split("/")[2];
+        if (!token.isEmpty()) {
+            String name = token.split("&")[0];
+            String pass = token.split("&")[1];
+
+            if (users.containsKey(name) && users.get(name).equals(pass) && authUsers.get(name)) {
+                webSocket.send("Hello, " + name + "!");
+            } else {
+                webSocket.send("Hello, u need to log in!");
+            }
+        } else {
+            webSocket.send("Hello, new user!");
+        }
     }
 
 
     public void onClose(WebSocket webSocket, int code, String reason, boolean remote) {
         cons.remove(webSocket); //delete from set
         System.out.println("Closed connection to: " + webSocket.getRemoteSocketAddress().getAddress().getHostAddress());
-        File file = new File("C:\\Users\\alexa\\IdeaProjects\\aacom.WebSocket\\src\\main\\java\\userip.txt");
 
     }
 
@@ -45,82 +57,41 @@ public class MyWebSocket extends WebSocketServer {
         String ipdir = "C:\\Users\\alexa\\IdeaProjects\\aacom.WebSocket\\src\\main\\java\\userip.txt";
 
         JSONObject obj = JSON.parseObject(message);
-        if (obj.getString("type").equals("registration")) {
-            String user = obj.getString("name");
-            String pass = obj.getString("pass");
-            String info = user + "," + pass;
-            String userip = user + "," + webSocket.getRemoteSocketAddress().getAddress().getHostAddress();
-            ipinfo = userip;
-            // registration
-            try (FileWriter fw1 = new FileWriter(filepath, true);
-                 BufferedWriter bw1 = new BufferedWriter(fw1);
-                 PrintWriter out1 = new PrintWriter(bw1))
-
-            {
-                out1.print(info + "\n");
-                webSocket.send("Welcome, " + user + "!");
-            } catch (IOException e) {
-                e.printStackTrace();
+        String type = obj.getString("type");
+        String user = obj.getString("name");
+        String pass = obj.getString("pass");
+        //System.out.println("type: " + type + "\nuser: " + user + "\npass: " + pass);
+        if (type.equals("registration")) {
+            if (!users.containsKey("user")) {
+                users.put(user, pass);
+                authUsers.put(user, true);
+                webSocket.send("{newKey:" + user + "%" + pass + "}");
+            } else {
+                webSocket.send("User Already Exists");
             }
-            //adding new ip
-            try (FileWriter fw = new FileWriter(ipdir, true);
-                 BufferedWriter bw = new BufferedWriter(fw);
-                 PrintWriter out = new PrintWriter(bw))
-
-            {
-                out.print(userip + "\n");
-                System.out.println("new ip: " + userip);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (obj.getString("type").equals("login")) {
-            String user = obj.getString("name");
-            String pass = obj.getString("pass");
-            String info = user + "," + pass;
-            String userip = user + "," + webSocket.getRemoteSocketAddress().getAddress().getHostAddress();
-            File file = new File(filepath);
-            //
-            try {
-                Scanner scanner = new Scanner(file);
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    if (line.equals(info)) {
-                        webSocket.send("Welcome back!");
-                    }
+        } else if (type.equals("login")) {
+            if (users.containsKey(user)) {
+                if (users.get(user).equals(pass)) {
+                    authUsers.put(user, true);
+                    webSocket.send("Logged in Successfully");
+                } else {
+                    webSocket.send("Wrong Password");
                 }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            //adding new ip
-            try (FileWriter fw = new FileWriter(ipdir, true);
-                 BufferedWriter bw = new BufferedWriter(fw);
-                 PrintWriter out = new PrintWriter(bw))
 
-            {
-                out.print(userip + "\n");
-                System.out.println("new ip: " + userip);
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else {
+                webSocket.send("User Invalid");
             }
+
         }
 
-        if (obj.getString("type").equals("message")) {
-            String user = obj.getString("name");
-            String text = obj.getString("text");
-            String info = user + ": " + text;
-
-            webSocket.send(info);
-        }
 
     }
 
-    public void onError(WebSocket webSocket, Exception e) {
+    public void onError (WebSocket webSocket, Exception e){
 
     }
 
-    public void onStart() {
+    public void onStart () {
 
     }
 
